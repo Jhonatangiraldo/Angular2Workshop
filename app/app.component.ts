1825d24dfa324b8aec4a46ffabdf7a048ed229d3
Jose Julian Betancur Nieto <jbnieto@gmail.com>
2016-12-01 11:21:46 -0500

    Load local variable and show it using ngFor directive

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Movie } from './movie';
+
+const MOVIES: Movie[] = [
+{ id: 1, title: "Pete's Dragon"},
+{ id: 2, title: "Moana"},
+{ id: 3, title: 'Solace' },
+{ id: 4, title: 'Passengers' },
+{ id: 5, title: 'Assassins Creed' },
+{ id: 6, title: 'Rogue One' },
+{ id: 7, title: 'Nocturnal Animals' },
+{ id: 8, title: 'Miss Peregrines' },
+{ id: 9, title: 'The Shining' },
+{ id: 10, title: 'Suicide Squad' }
+];
+
 @Component({
 	selector: 'my-app',
 	templateUrl: 'app/app.component.html',
@@ -8,6 +23,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent { 
 	name: string;
+	movies = MOVIES;
 	constructor() {
 		this.name = 'Other World';
 	}
