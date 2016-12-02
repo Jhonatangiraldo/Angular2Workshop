f823ea72a31c461590e85c87c7dbb50e32cc414d
Jose Julian Betancur Nieto <jbnieto@gmail.com>
2016-12-02 10:00:13 -0500

    Change rate usign rating component

--- app/rating/rating.component.ts
+++ app/rating/rating.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter } from '@angular/core';
 
 @Component({
 	selector: 'rating',
 	templateUrl: 'app/rating/rating.component.html',
-	styleUrls: ['./app/styles/rating/styles.css']
+	styleUrls: ['./app/styles/rating/styles.css'],
+	outputs: ['updateRate: rateChange']
 })
 export class Rating {
 	@Input() rate: number;
 	private range:Array<number> = [1,2,3,4,5];
+	private updateRate = new EventEmitter();
 	constructor() {		
+	}
+	update(value: number) {
+		console.log('Stars: ' + value);
+		this.rate = value;
+		this.updateRate.next(value);
 	}
 }
